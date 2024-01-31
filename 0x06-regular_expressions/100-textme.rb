975255910a4c01b958340da8e66101082ebb89a5
euGenius29 <angloweugenious29@gmail.com>
2024-01-31 07:03:58 -0800

    error corrections in advanced task

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # This script matches sender, receiver phone numbers or names, and then flags.
 
-puts ARGV[0].scan(/\[from:(.*?)\]\s[to:(.*?)]\s[flags:(.*?)\]/).join(',')
+puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)]\s\[flags:(.*?)\]/).join(',')
